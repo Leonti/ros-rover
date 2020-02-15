16314a4596d5d449d9307bfce1954823e3053d12
Leonti <prishelec@gmail.com>
2020-02-15 12:26:41 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+## Install
+
+Install instructions for Ubuntu Bionic.
+
+1. Install the appropriate ROS 2 version as instructed [here](https://index.ros.org/doc/ros2/Installation/Linux-Install-Debians/).
+
+1. Install `gazebo_ros_pkgs`
+
+1. Clone repo:
+
+        mkdir -p ~/ws/src
+        cd ~/ws/src
+        git clone https://github.com/Leonti/ros-rover
+
+1. Build and install:
+
+        cd ~/ws
+        colcon build --symlink-install
+
+## Run
+
+1. Setup environment variables:
+
+        . /usr/share/gazebo/setup.sh
+        . /home/leonti/ros2_eloquent/install/setup.bash
+        . /home/leonti/development/ros2_tutorial/install/setup.bash
+
+2. Launch rover:
+
+        ros2 launch rover_gazebo rover.launch.py world:=rover_room.world
+
+3. In `slam_toolbox` worspace run:
+ 
+        . /home/leonti/ros2_eloquent/install/setup.bash
+        . /home/leonti/slam_ws/install/setup.bash
+4. Launch `slam_toolbox`:
+        ros2 launch slam_toolbox online_async_launch.py
+
+
+## Credits
+This project is based on the awesome "Dolly" project: https://github.com/chapulina/dolly
+     
---
+++ rover_gazebo/launch/rover.launch.py
@@ -0,0 +1,44 @@
+"""Launch Gazebo with a world that has rover, as well as the follow node."""
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_rover_gazebo = get_package_share_directory('rover_gazebo')
+
+    # Gazebo launch
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py'),
+        )
+    )
+
+    # RViz
+    rviz = Node(
+        package='rviz2',
+        node_executable='rviz2',
+        arguments=['-d', os.path.join(pkg_rover_gazebo, 'rviz', 'rover_gazebo.rviz')],
+        condition=IfCondition(LaunchConfiguration('rviz'))
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+          'world',
+          default_value=[os.path.join(pkg_rover_gazebo, 'worlds', 'rover_empty.world'), ''],
+          description='SDF world file'),
+        DeclareLaunchArgument('rviz', default_value='true',
+                              description='Open RViz.'),
+        gazebo,
+        rviz
+    ])
