f4a0e56729e2fb348b13751991af88500ef33de9
Leonti Bielski <lbielski@seek.com.au>
2020-03-04 13:11:07 +1100

    Create arduino folder

---
+++ arduino/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+void receiveEvent(int howMany) {
+  while(Wire.available()) {
+    char c = Wire.read();
+  }
+}
+
+void setup() {
+  Wire.begin(4);
+  Wire.onReceive(receiveEvent);
+}
+
+void loop() {
+  delay(100);
+}
