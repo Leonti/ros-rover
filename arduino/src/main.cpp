ff6c88a382f5adbdb8507f5118a3edd2c3f2b2eb
Leonti Bielski <lbielski@seek.com.au>
2020-03-05 13:06:58 +1100

    Add libraries, starter code for motors

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,6 +1,60 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <PID_v1.h>
+#include <SparkFun_TB6612.h>
+
+#define AIN1 2
+#define BIN1 7
+#define AIN2 4
+#define BIN2 8
+#define PWMA 5
+#define PWMB 6
+#define STBY 9
+
+const int offsetA = 1;
+const int offsetB = 1;
+
+Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
+Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
+
+
+unsigned long lastMilli = 0;
+const double radius = 0.04;                   //Wheel radius, in m
+const double wheelbase = 0.187;               //Wheelbase, in m
+
+double speed_req = 0;                         //Desired linear speed for the robot, in m/s
+double angular_speed_req = 0;                 //Desired angular speed for the robot, in rad/s
+
+double speed_req_left = 0;                    //Desired speed for left wheel in m/s
+double speed_act_left = 0;                    //Actual speed for left wheel in m/s
+double speed_cmd_left = 0;                    //Command speed for left wheel in m/s 
+
+double speed_req_right = 0;                   //Desired speed for right wheel in m/s
+double speed_act_right = 0;                   //Actual speed for right wheel in m/s
+double speed_cmd_right = 0;                   //Command speed for right wheel in m/s 
+                        
+const double max_speed = 0.4;                 //Max speed in m/s
+
+int PWM_leftMotor = 0;                     //PWM command for left motor
+int PWM_rightMotor = 0;                    //PWM command for right motor 
+
+// PID Parameters
+const double PID_left_param[] = { 0, 0, 0.1 }; //Respectively Kp, Ki and Kd for left motor PID
+const double PID_right_param[] = { 0, 0, 0.1 }; //Respectively Kp, Ki and Kd for right motor PID
+
+volatile float pos_left = 0;       //Left motor encoder position
+volatile float pos_right = 0;      //Right motor encoder position
+
+PID PID_leftMotor(&speed_act_left, &speed_cmd_left, &speed_req_left, PID_left_param[0], PID_left_param[1], PID_left_param[2], DIRECT);          //Setting up the PID for left motor
+PID PID_rightMotor(&speed_act_right, &speed_cmd_right, &speed_req_right, PID_right_param[0], PID_right_param[1], PID_right_param[2], DIRECT);   //Setting up the PID for right motor
+
+const int PIN_ENCOD_A_MOTOR_LEFT = 2;               //A channel for encoder of left motor                    
+const int PIN_ENCOD_B_MOTOR_LEFT = 4;               //B channel for encoder of left motor
+
+const int PIN_ENCOD_A_MOTOR_RIGHT = 3;              //A channel for encoder of right motor         
+const int PIN_ENCOD_B_MOTOR_RIGHT = 5;              //B channel for encoder of right motor 
+
+#define LOOPTIME                      100
 
 void receiveEvent(int howMany) {
   while(Wire.available()) {
@@ -8,11 +62,97 @@ void receiveEvent(int howMany) {
   }
 }
 
+//Left motor encoder counter
+void encoderLeftMotor() {
+  if (digitalRead(PIN_ENCOD_A_MOTOR_LEFT) == digitalRead(PIN_ENCOD_B_MOTOR_LEFT)) pos_left++;
+  else pos_left--;
+}
+
+//Right motor encoder counter
+void encoderRightMotor() {
+  if (digitalRead(PIN_ENCOD_A_MOTOR_RIGHT) == digitalRead(PIN_ENCOD_B_MOTOR_RIGHT)) pos_right--;
+  else pos_right++;
+}
+
 void setup() {
+
+  PID_leftMotor.SetSampleTime(95);
+  PID_rightMotor.SetSampleTime(95);
+  PID_leftMotor.SetOutputLimits(-max_speed, max_speed);
+  PID_rightMotor.SetOutputLimits(-max_speed, max_speed);
+  PID_leftMotor.SetMode(AUTOMATIC);
+  PID_rightMotor.SetMode(AUTOMATIC);
+
+
+  // Define the rotary encoder for left motor
+  pinMode(PIN_ENCOD_A_MOTOR_LEFT, INPUT); 
+  pinMode(PIN_ENCOD_B_MOTOR_LEFT, INPUT); 
+  digitalWrite(PIN_ENCOD_A_MOTOR_LEFT, HIGH);                // turn on pullup resistor
+  digitalWrite(PIN_ENCOD_B_MOTOR_LEFT, HIGH);
+  attachInterrupt(0, encoderLeftMotor, RISING);
+
+  // Define the rotary encoder for right motor
+  pinMode(PIN_ENCOD_A_MOTOR_RIGHT, INPUT); 
+  pinMode(PIN_ENCOD_B_MOTOR_RIGHT, INPUT); 
+  digitalWrite(PIN_ENCOD_A_MOTOR_RIGHT, HIGH);                // turn on pullup resistor
+  digitalWrite(PIN_ENCOD_B_MOTOR_RIGHT, HIGH);
+  attachInterrupt(1, encoderRightMotor, RISING);
+
   Wire.begin(4);
   Wire.onReceive(receiveEvent);
 }
 
+template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+
 void loop() {
-  delay(100);
+
+  if((millis()-lastMilli) >= LOOPTIME) {
+    lastMilli = millis();
+    
+    if (abs(pos_left) < 5){                                                   //Avoid taking in account small disturbances
+      speed_act_left = 0;
+    } else {
+      speed_act_left=((pos_left/990)*2*PI)*(1000/LOOPTIME)*radius;           // calculate speed of left wheel
+    }
+    
+    if (abs(pos_right) < 5){                                                  //Avoid taking in account small disturbances
+      speed_act_right = 0;
+    } else {
+      speed_act_right=((pos_right/990)*2*PI)*(1000/LOOPTIME)*radius;          // calculate speed of right wheel
+    }
+    
+    pos_left = 0;
+    pos_right = 0;
+
+    speed_cmd_left = constrain(speed_cmd_left, -max_speed, max_speed);
+    PID_leftMotor.Compute();                                                 // compute PWM value for left motor
+    PWM_leftMotor = constrain(((speed_req_left+sgn(speed_req_left)*0.0882)/0.00235) + (speed_cmd_left/0.00235), -255, 255); //
+    
+    if (speed_req_left == 0){                        //Stopping
+
+    } else if (PWM_leftMotor > 0){                          //Going forward
+    //  leftMotor->setSpeed(abs(PWM_leftMotor));
+     // leftMotor->run(BACKWARD);
+    } else {                                               //Going backward
+   //   leftMotor->setSpeed(abs(PWM_leftMotor));
+   //   leftMotor->run(FORWARD);
+    }
+    
+    speed_cmd_right = constrain(speed_cmd_right, -max_speed, max_speed);    
+    PID_rightMotor.Compute();                                                 // compute PWM value for right motor
+    PWM_rightMotor = constrain(((speed_req_right+sgn(speed_req_right)*0.0882)/0.00235) + (speed_cmd_right/0.00235), -255, 255); // 
+
+    if (speed_req_right == 0){                       //Stopping
+     // rightMotor->setSpeed(0);
+     // rightMotor->run(BRAKE);
+    } else if (PWM_rightMotor > 0){                         //Going forward
+    //  rightMotor->setSpeed(abs(PWM_rightMotor));
+     // rightMotor->run(FORWARD);
+    } else {                                                //Going backward
+    //  rightMotor->setSpeed(abs(PWM_rightMotor));
+    //  rightMotor->run(BACKWARD);
+    }
+  }
 }
