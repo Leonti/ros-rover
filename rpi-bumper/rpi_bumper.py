33b2a1d9c66e0dc24e3a4b17f9fb3815b9240007
Leonti <prishelec@gmail.com>
2020-08-17 15:35:17 +1000

    remember the value of another bumper

--- rpi-bumper/rpi_bumper.py
+++ rpi-bumper/rpi_bumper.py
@@ -48,30 +48,38 @@ class BumperPublisher(Node):
     self.publisher_ = self.create_publisher(
         Bumper, '/bumper', qos_profile=qos_profile_services_default)
 
+    self.left = False
+    self.right = False
+
     GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    left_bumper_cb = ButtonHandler(17, self.on_left_bumper, bouncetime=60)
+    left_bumper_cb = ButtonHandler(17, self.on_left_bumper, bouncetime=40)
     left_bumper_cb.start()
     GPIO.add_event_detect(17, GPIO.BOTH, callback=left_bumper_cb)
 
     GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    right_bumper_cb = ButtonHandler(27, self.on_right_bumper, bouncetime=60)
+    right_bumper_cb = ButtonHandler(27, self.on_right_bumper, bouncetime=40)
     right_bumper_cb.start()
     GPIO.add_event_detect(27, GPIO.BOTH, callback=right_bumper_cb)
 
   def on_left_bumper(self, value):
+
+    self.left = value
+
     msg = Bumper()
     msg.center = False
-    msg.left = value
-    msg.right = False
+    msg.left = self.left
+    msg.right = self.right
 
     self.publisher_.publish(msg)
     self.get_logger().info('Publishing: "%s,%s,%s"' % (msg.left, msg.center, msg.right))
 
   def on_right_bumper(self, value):
+    self.right = value
+
     msg = Bumper()
     msg.center = False
-    msg.left = False
-    msg.right = value
+    msg.left = self.left
+    msg.right = self.right
 
     self.publisher_.publish(msg)
     self.get_logger().info('Publishing: "%s,%s,%s"' % (msg.left, msg.center, msg.right))
